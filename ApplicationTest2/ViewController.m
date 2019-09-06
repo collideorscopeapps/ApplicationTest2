7015c6fc1199aa952096a4d29daf5bbfe94fc3e7
collideorscopeapps <mauro81@gmail.com>
2019-09-06 15:44:11 +0200

    implementati metodi per la table view.

--- ApplicationTest2/ViewController.m
+++ ApplicationTest2/ViewController.m
@@ -18,7 +18,29 @@
 
 #pragma mark - TableView Delegate and Datasource
 
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    
+    return 1;
+}
 
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    
+    return people.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView
+         cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cellID" forIndexPath:indexPath];
+    
+    NSDictionary *person = people[indexPath.row];
+    
+    cell.textLabel.text = [NSString stringWithFormat:@"%@ %@", person[@"name"], person[@"lastName"]];
+    
+    cell.detailTextLabel.text = person[@"address"];
+    
+    return cell;
+}
 
 #pragma mark - Lifecycle
 
